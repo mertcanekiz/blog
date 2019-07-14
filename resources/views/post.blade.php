0f1acb9ec427189d834d5759944ac128e7e470b3
Mertcan Ekiz <ekiz.mertcan@gmail.com>
2019-07-14 14:16:56 +0300

    Added profile links to posts

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -2,7 +2,7 @@
     <div class="card w-100 mb-3">
         <div class="card-body">
             <h5 class="card-title">{{ $post->title }}</h5>
-            <h6 class="card-subtitle mb-2 text-muted">{{ $post->user->username }}</h6>
+            <a href="{{ route('profileWithUsername', ['username' => $post->user->username]) }}"><h6 class="card-subtitle mb-2 text-muted">{{ $post->user->username }}</h6></a>
             <p class="card-text">{{ $post->content }}</p>
             <a href="#" class="card-link">Card link</a>
             <a href="#" class="card-link">Another link</a>
