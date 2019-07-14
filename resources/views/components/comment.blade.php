c403139fbb47811d661f1d507dce2ab87b9c2b98
Mertcan Ekiz <ekiz.mertcan@gmail.com>
2019-07-14 23:57:40 +0300

    Changed navbar style

--- resources/views/components/comment.blade.php
+++ resources/views/components/comment.blade.php
@@ -1,5 +1,5 @@
 <div class="row">
     <div class="col">
-        <a class="text-dark" href="{{ route('profileWithUsername', ['username' => $comment->user->username]) }}"><strong>{{ $comment->user->username }}</strong></a> {{ $comment->content }}
+        <a href="{{ route('profileWithUsername', ['username' => $comment->user->username]) }}"><strong>{{ $comment->user->username }}</strong></a> {{ $comment->content }}
     </div>
 </div>
