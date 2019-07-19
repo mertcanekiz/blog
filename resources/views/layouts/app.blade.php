2fcac1309a7772f0adf0dbeb5c277fb1183098e4
Mertcan Ekiz <ekiz.mertcan@gmail.com>
2019-07-19 11:14:07 +0300

    Changed <p> to <div> in post component

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -116,10 +116,6 @@
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}"></script>
     <script src="{{ asset('js/script.js') }}"></script>
-    <script
-            src="https://code.jquery.com/jquery-3.4.1.min.js"
-            integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="
-            crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/summernote/0.8.12/summernote-lite.js"></script>
 </body>
 </html>
